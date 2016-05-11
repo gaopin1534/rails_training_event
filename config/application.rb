c34a811c46e04834978188bc1f51425874a2e5f4
高松　幸平 <01014501@CA0945.local>
2016-05-11 15:12:50 +0900

    change setting to use haml and sass

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,11 @@ module TwiEvent
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators do |g|
+      g.template_engine :haml
+    end
+
+    config.sass.preferrd_syntax = :sass
+    config.sass.syntax = :sass
   end
 end
