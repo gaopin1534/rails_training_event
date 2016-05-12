12dcdd99871f6e62612ac147ef6f425bbf21ae38
高松　幸平 <01014501@CA0945.local>
2016-05-12 16:30:35 +0900

    add event delete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -26,6 +26,12 @@ class EventsController < ApplicationController
     end
   end
 
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to root_path, notice: "event was deleted!"
+  end
+
   def update
     @event = Event.find(params[:id])
     if @event.update_attributes(event_params)
