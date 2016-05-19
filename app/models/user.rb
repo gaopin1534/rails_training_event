69cbeac64e781bd80f0b2e73fecce03791077cfd
高松　幸平 <01014501@CA0945.local>
2016-05-19 20:23:14 +0900

    fix omniauth settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,13 +18,12 @@ class User < ActiveRecord::Base
     self.secret = auth.credentials.secret
     self.provider = auth.provider
     uploader = ImageUploader.new
+    self.description = auth.info.description
     if(auth.provider == "facebook")
-      self.description = auth.info.about
-      uploader.download!  "https://graph.facebook.com/#{auth.uid}/picture?type=square"
       self.nickname = auth.info.email
+      uploader.download!  "https://graph.facebook.com/#{auth.uid}/picture?type=square"
     else
       self.nickname = auth.info.nickname
-      self.description = auth.info.description
       uploader.download!(auth.info.image)
     end
     self.image = uploader
@@ -33,12 +32,11 @@ class User < ActiveRecord::Base
   def get_data_from_auth_info_for_update auth
     self.name = auth.info.name
     self.uid = auth.uid
+    self.description = auth.info.about
     if(auth.provider == "facebook")
-      self.description = auth.info.about
       self.nickname = auth.info.email
     else
       self.nickname = auth.info.nickname
-      self.description = auth.info.description
     end
     self.token = auth.credentials.token
     self.secret = auth.credentials.secret
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Settings.facebook_key, Settings.facebook_secret,
                            image_size: { width: 500, height: 500 },
                            secure_image_url: true,
-                           info_fields:"name,email,about"
+                           info_fields:"name,email,about,bio",
+                           scope:"user_about_me,email"
   on_failure do |env|
     # 認証をキャンセルしたりだとかした場合等に/auth/failureにぶっ飛ばす
     OmniAuth::FailureEndpoint.new(env).redirect_to_failure
